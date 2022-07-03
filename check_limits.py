cb462fe5b005791320230c225123ad75d5510d59
Rahul Kumar <Rahulkr9622@gmail.com>
2022-07-03 13:53:00 +0530

    Fix CCN

--- check_limits.py
+++ check_limits.py
@@ -8,7 +8,9 @@ def battery_is_ok(temperature_val, soc_val, charge_rate_val):
     is_temp_ok = Temperature.verify_operating_temperature_with_battery(temperature_val)
     is_soc_ok = State_Of_Charge.verify_soc_battery(soc_val)
     is_charge_rate_ok = Charge_Rate.verify_charge_rate(charge_rate_val)
+    return test_all_factors(is_temp_ok, is_soc_ok, is_charge_rate_ok)
+
+def test_all_factors(is_temp_ok, is_soc_ok, is_charge_rate_ok):
     if(is_temp_ok and is_soc_ok and is_charge_rate_ok):
         return True
-    else:
-        return False
+    return False
